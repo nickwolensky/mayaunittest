c760aaa27e0976ab7f6f1e85af99eb34e798b991
Nick Wolensky <nicholas.wolensky@gmail.com>
2017-09-08 00:34:15 -0700

    More functionality for the window. Start directory at root. Add context menu right click to move up directory. Added a button to do the same thing.

--- testcode_inspect_ui.py
+++ testcode_inspect_ui.py
@@ -7,9 +7,9 @@ Todo:
 """
 import os
 import sys
-path = 'C:/00_git/bpt-artists/src/'
-if path not in sys.path:
-    sys.path.append(path)
+# path = 'C:/00_git/bpt-artists/src/'
+# if path not in sys.path:
+#     sys.path.append(path)
 
 from nw_tools.Qt import QtWidgets, QtGui, QtCore
 from nw_tools.ui.tools import get_maya_window, SuperWindow
@@ -20,9 +20,19 @@ class Tree(QtWidgets.QTreeView):
         super(Tree, self).__init__(parent)
 
         self.parent = parent
+        self.create_actions()
+
+    def create_actions(self):
+        self.walk_up_action = QtWidgets.QAction('Walk up directory', self)
+        self.walk_up_action.triggered.connect(self.parent.walk_up)
 
     def mouseDoubleClickEvent(self, event):
-        self.parent.update_system_tree()
+        self.parent.walk_down()
+
+    def contextMenuEvent(self, event):
+        menu = QtWidgets.QMenu(self)
+        menu.addAction(self.walk_up_action)
+        menu.exec_(event.globalPos())
 
 
 class TestCodeUI(SuperWindow):
@@ -34,6 +44,7 @@ class TestCodeUI(SuperWindow):
     DOCKABLE = True
 
     def __init__(self, parent=None):
+        self.current_dir = QtCore.QDir.rootPath()
         super(TestCodeUI, self).__init__(parent)
 
     def _init_ui(self):
@@ -56,37 +67,56 @@ class TestCodeUI(SuperWindow):
     def _add_widgets(self):
         split = QtWidgets.QSplitter(self.centralWidget())
         split.resize(self.WIDTH, self.HEIGHT)
-        split.setHandleWidth(2)
+        split.setHandleWidth(3)
+        split.setContentsMargins(2, 2, 2, 2)
+
+        btn_widg = QtWidgets.QWidget()
+        vbox = QtWidgets.QVBoxLayout()
+        btn_row = QtWidgets.QHBoxLayout()
+        btn_row.setContentsMargins(0, 0, 0, 0)
+        btn_widg.setLayout(vbox)
+        vbox.setContentsMargins(0, 0, 2, 0)
 
         # Create a tree outliner for package that I want to recursively go
         # through and test | Left Panel
         self.model = QtWidgets.QFileSystemModel()
-        self.model.setRootPath(QtCore.QDir.currentPath())
+        self.model.setRootPath(self.current_dir)
 
-        # self.tree = QtWidgets.QTreeView()
         self.tree = Tree(self)
         self.tree.setModel(self.model)
         self.tree.hideColumn(1)
         self.tree.hideColumn(2)
         self.tree.hideColumn(3)
 
+        self._update_system_tree(self.current_dir)
+
         # Create output section that tests get printed out to | Right Panel
         text_display = QtWidgets.QTextEdit('This is going to be the place '
                                            'where I display output from the '
                                            'unittests')
 
-        split.addWidget(self.tree)
+        self.btn = QtWidgets.QPushButton('^')
+        self.btn.setFixedWidth(25)
+        btn_row.addWidget(self.btn)
+        btn_row.addStretch(0)
+        vbox.addLayout(btn_row)
+        vbox.addWidget(self.tree)
+
+        split.addWidget(btn_widg)
         split.addWidget(text_display)
         split.setStretchFactor(0, 0)
         split.setStretchFactor(1, 2)
 
     def _add_signals(self):
         # Open command
-        self.open_dir_action.triggered.connect(self.update_system_tree)
+        # self.open_dir_action.triggered.connect(self.update_system_tree)
         # Run command
         self.run_action.triggered.connect(self.get_selected_dir)
         # Stop command
-        self.stop_action.triggered.connect(self.dir_up)
+        # self.stop_action.triggered.connect(self.dir_up)
+
+        # Move up button
+        self.btn.clicked.connect(self.walk_up)
 
     def get_selected_dir(self):
         index = self.tree.currentIndex()
@@ -94,25 +124,20 @@ class TestCodeUI(SuperWindow):
 
     def _update_system_tree(self, directory):
         self.tree.setRootIndex(self.model.index(directory))
+        self.current_dir = directory
 
-    def update_system_tree(self):
-        self._update_system_tree(self.get_selected_dir())
-
-    def dir_up(self):
-        par_dir = os.path.abspath(os.path.join(self.get_selected_dir(),
-                                               os.pardir))
-        print par_dir
+    def walk_up(self):
+        par_dir = os.path.abspath(os.path.join(self.current_dir, os.pardir))
         self._update_system_tree(par_dir)
+
+    def walk_down(self):
+        self._update_system_tree(self.get_selected_dir())
 
     def open_file_dialog(self):
         pass
 
     def dragEnterEvent(self, event):
         pass
-
-    def mouseDoubleClickEvent(self, event):
-        print 'hello'
-        self.update_system_tree()
 
 
 if __name__ == '__main__':
